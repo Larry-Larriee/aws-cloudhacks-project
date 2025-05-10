84838cb8605ba15d9e02acc10c081a548cdc6386
Larry Le <larryle704@gmail.com>
2025-05-10 13:10:59 -0700

    initial build

---
+++ my-app/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex w-full h-full justify-center">
+      <p className="text-3xl">hi</p>
+    </div>
+  );
+}
